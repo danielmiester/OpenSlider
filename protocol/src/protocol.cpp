0524616395bec6706b2bab89ef85bc8b9084ced3
Daniel De Jager <danielmiester@gmail.com>
2015-05-07 10:55:49 -0700

    Update protocol.cpp

--- protocol/src/protocol.cpp
+++ protocol/src/protocol.cpp
@@ -1,6 +1,13 @@
 #include "Arduino.h"
 #include "protocol.h"
+#define IDLE 	0
+#define FOUND_VERB 1
+#define FOUND_ADDR 2
+#define FOUND_ADDR_DELIM 3
+#define FOUND_DATA 4
+#define FOUND_DATA_DELIM 5
 
+//#123:123,123;
 Protocol::Protocol(address_t addr){
   _buf = malloc(PROTOCOL_BUF_SIZE);
   _address = addr;
@@ -11,4 +18,15 @@ Protocol::inputData(size_t len, char* data){
 Protocol::setAddress(address_t addr){
 	_address = addr;
 }
-Protocol::processData(void){}
+Protocol::stateMachine(char data){
+	switch(state){
+	case IDLE:
+		if(data){}	
+	}
+}
+Protocol::processData(char data){
+	this->stateMachine(data);
+	switch(state){
+	case 
+	}
+}

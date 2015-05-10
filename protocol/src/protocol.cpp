a9337eb1d8a2dd58f5f98a6ccc7bd47d710b9b05
Daniel De Jager <danielmiester@gmail.com>
2015-05-10 16:01:56 -0700

    Update protocol.cpp

--- protocol/src/protocol.cpp
+++ protocol/src/protocol.cpp
@@ -1,11 +1,6 @@
 #include "Arduino.h"
 #include "protocol.h"
-#define IDLE 	0
-#define FOUND_VERB 1
-#define FOUND_ADDR 2
-#define FOUND_ADDR_DELIM 3
-#define FOUND_DATA 4
-#define FOUND_DATA_DELIM 5
+
 
 //#123:123,123;
 Protocol::Protocol(address_t addr){
@@ -20,8 +15,13 @@ Protocol::setAddress(address_t addr){
 }
 Protocol::stateMachine(char data){
 	switch(state){
-	case IDLE:
-		if(data){}	
+	case Protocol::STATE::IDLE:
+	    if(_prev_char != '\'){
+    		switch(data){
+    		    case Protocol::VERB::QUERY:break;
+    		    
+    		}
+	    }
 	}
 }
 Protocol::processData(char data){
